3942df39e3e48610c4852830f6110179e25ea55d
Nyingambe Brian <briannmbe@gmail.com>
2023-11-21 23:27:24 +0300

    try lorem

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,10 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi quas
+      delectus dolorum culpa. Voluptatibus possimus libero, laborum provident ex
+      unde, quidem, assumenda beatae corporis eaque voluptates exercitationem
+      nihil? Tenetur.
+    </div>
+  );
 }
